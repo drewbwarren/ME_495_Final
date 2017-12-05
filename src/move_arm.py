e18a34b443e4e999e6d85bfcff2f9732d499d804
Andrew <andrewwentzel2023@u.northwestern.edu>
2017-12-05 15:50:53 -0600

    have move_arm.py working better

--- src/move_arm.py
+++ src/move_arm.py
@@ -1,45 +1,5 @@
 #!/usr/bin/env python
 
-# https://github.com/ros-planning/moveit_pr2/blob/hydro-devel/pr2_moveit_tutorials/planning/scripts/move_group_python_interface_tutorial.py
-
-# Software License Agreement (BSD License)
-#
-# Copyright (c) 2013, SRI International
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-#  * Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-#  * Redistributions in binary form must reproduce the above
-#    copyright notice, this list of conditions and the following
-#    disclaimer in the documentation and/or other materials provided
-#    with the distribution.
-#  * Neither the name of SRI International nor the names of its
-#    contributors may be used to endorse or promote products derived
-#    from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#
-# Author: Acorn Pooley
-
-## BEGIN_SUB_TUTORIAL imports
-##
-## To use the python interface to move_group, import the moveit_commander
-## module.  We also import rospy and some messages that we will use.
 import sys
 import copy
 import rospy
@@ -48,11 +8,13 @@ import moveit_msgs.msg
 import geometry_msgs.msg
 import baxter_interface
 import tf
+import math
 ## END_SUB_TUTORIAL
 
 from std_msgs.msg import (Header, String, UInt16)
 from geometry_msgs.msg import (PoseStamped, Pose, Point, Quaternion)
-from moveit_msgs.msg import (Constraints, OrientationConstraint)
+from moveit_msgs.msg import (Constraints, OrientationConstraint, CollisionObject)
+from shape_msgs.msg import SolidPrimitive
 from baxter_interface import CHECK_VERSION
 
 from moveit_commander import MoveGroupCommander
@@ -60,27 +22,68 @@ from moveit_commander import MoveGroupCommander
 class MoveCup():
 
     def __init__(self):
+        #basic initiatioon
         moveit_commander.roscpp_initialize(sys.argv)
         rospy.init_node('moveit_tutorial')
         self.robot = moveit_commander.RobotCommander()
+        ################ Collision Object
         self.scene = moveit_commander.PlanningSceneInterface()
+        table = CollisionObject()
+        primitive = SolidPrimitive()
+        primitive.type = primitive.BOX
+        box_pose = geometry_msgs.msg.PoseStamped()
+        box_pose.header.stamp = rospy.Time.now()
+        box_pose.header.frame_id = 'tablelol'
+        box_pose.pose.position.x = 1.5
+        box_pose.pose.position.y = 0.0
+        box_pose.pose.position.z = 0.0
+        table.primitives.append(primitive)
+        table.primitive_poses.append(box_pose)
+        table.operation = table.ADD
+        self.scene.add_box('table',box_pose,size=(.077,.073,.122))
         #use joint_group parameter to change which arm it uses?
-        joint_group = rospy.get_param('~arm', default="left_arm")
-        self.group = MoveGroupCommander(joint_group)
-        self.p = PoseStamped()
-        self.p.header.frame_id = self.robot.get_planning_frame()
+        self.joint_group = rospy.get_param('~arm', default="left_arm")
+        self.group = MoveGroupCommander(self.joint_group)
+        #this node will scale any tf pose requests to be at most max_reach from the base frame
+        self.max_reach = rospy.get_param('~max_reach', default=1.1)
+        #define a start pose that we can move to before stuff runs
+        self.start_pose = PoseStamped()
+        self.start_pose = self.get_start_pose()
         #remove this when working for realz
         self.display_publisher = rospy.Publisher('/move_group/display_planned_path',moveit_msgs.msg.DisplayTrajectory,queue_size=10)
+        self.rate = rospy.Rate(1)
+
+    def callback(self, targetstamped):
+        #callback that moves in a constrained path to anything published to /target_poses
+        ##First, scale the position to be withing self.max_reach
+        target = targetstamped.pose
+        new_target = self.project_point(target.position)
+        target.position = new_target
+        #change orientation to be upright
+        target.orientation = self.start_pose.pose.orientation
+        #clear group info and set it again
+        self.group.clear_pose_targets()
+        self.group.set_path_constraints(self.get_constraint())
+        self.group.set_planning_time(15)
+        self.group.set_pose_target(target)
+        #plan and execute plan. If I find a way, I should add error checking her
+        #currently, if the plan fails, it just doesn't move and waits for another pose to be published
+        plan = self.group.plan()
+        self.group.execute(plan)
+        self.rate.sleep()
+        return
 
     def scale_movegroup(self,vel = .4,acc = .9):
+        #slows down baxters arm so we stop getting all those velocity limit errors
         self.group.set_max_velocity_scaling_factor(vel)
         self.group.set_max_acceleration_scaling_factor(acc)
 
     def unscale_movegroup(self):
-        self.group.set_max_velocity_scaling_factor(0)
-        self.group.set_max_acceleration_scaling_factor(0)
+        self.group.set_max_velocity_scaling_factor(1)
+        self.group.set_max_acceleration_scaling_factor(1)
 
     def start_baxter_interface(self):
+        #I copied this from an example but have no idea what it does
         self._pub_rate = rospy.Publisher('robot/joint_state_publish_rate',
                                          UInt16, queue_size=10)
         self._left_arm = baxter_interface.limb.Limb("left")
@@ -104,62 +107,48 @@ class MoveCup():
         print('Is neutral now')
         return
 
-    def set_p(self,x=.05,y=0,z=0,ox=0,oy=0,oz=0,w=1):
+    def get_start_pose(self,point=[1, 0.2, 0.2],rpy=[0, math.pi/2, 0]):
+        #define a starting position for the move_start method
         new_p = PoseStamped()
         new_p.header.frame_id = self.robot.get_planning_frame()
-        new_p.p.pose.position.x = x
-        new_p.p.pose.position.y = y
-        new_p.p.pose.position.z = z
+        new_p.pose.position.x = point[0]
+        new_p.pose.position.y = point[1]
+        new_p.pose.position.z = point[2]
 
-        new_p.p.pose.orientation.x = ox
-        new_p.p.pose.orientation.y = oy
-        new_p.p.pose.orientation.z = oz
-        new_p.p.pose.orientation.w = w
-        self.p = new_p
-        rospy.spin()
+        p_orient = tf.transformations.quaternion_from_euler(rpy[0],rpy[1],rpy[2])
+        p_orient = Quaternion(*p_orient)
+        new_p.pose.orientation = p_orient
         return(new_p)
 
-    def get_end_pose(self):
-        end_pose_dict = self._left_arm.endpoint_pose()
-        end_pose = Pose()
-        end_pose.position = end_pose_dict['position']
-        end_pose.orientation = end_pose_dict['orientation']
-        print('end pose:')
-        print(end_pose)
-        return(end_pose)
-
-    def set_group_start_to_current_pose(self):
-        self.group.set_start_state(self.get_end_pose())
-
-    def set_target(self,x=-.5,y=-.5,z=.6,ox=.6,oy=.4,oz=0,w=0):
-        target = PoseStamped()
-        target.header.frame_id = self.robot.get_planning_frame()
-        target.pose.position.x = x
-        target.pose.position.y = y
-        target.pose.position.z = z
-
-        target.pose.orientation.x = ox
-        target.pose.orientation.y = oy
-        target.pose.orientation.z = oz
-        target.pose.orientation.w = w
-        self.group.set_pose_target(target)
-        return(target)
+    def project_point(self,point):
+        #scales a Point() (see: Pose.position) to be within self.max_reach
+        obj_dist = math.sqrt(point.x**2 + point.y**2 + point.z**2)
+        scale_val = min(self.max_reach/obj_dist,.99)
+        point_scaled = Point()
+        point_scaled.x = scale_val*point.x
+        point_scaled.y = scale_val * point.y
+        point_scaled.y = scale_val * point.y
+        return(point_scaled)
 
     def move_random(self):
-        self.set_group_start_to_current_pose()
+        #moves baxter to a random position.  used for testing
         randstate = PoseStamped()
         randstate = self.group.get_random_pose()
         self.group.clear_pose_targets()
         self.group.set_pose_target(randstate)
         self.group.set_planning_time(10)
         self.scale_movegroup()
-        self.group.plan()
-        self.group.go()
-        rospy.spin()
+        plan = self.group.plan()
+        while len(plan.joint_trajectory.points) == 1 and not rospy.is_shutdown():
+            print('plan no work')
+            plan = self.group.plan()
+        self.group.execute(plan)
+        self.rate.sleep()
         return
 
     def move_random_constrained(self):
-        self.set_group_start_to_current_pose()
+        #move baxter to a random position with constrained path planning.  also for testing
+        self.scale_movegroup()
         randstate = PoseStamped()
         randstate = self.group.get_random_pose()
         self.group.clear_pose_targets()
@@ -169,37 +158,29 @@ class MoveCup():
         self.scale_movegroup()
         constrained_plan = self.group.plan()
         self.group.execute(constrained_plan)
-        rospy.spin()
+        self.unscale_movegroup()
+        rospy.sleep(3)
         return
 
-    def move_target(self):
+    def move_start(self):
+        #move baxter to the self.start_pose pose
         self.group.clear_pose_targets()
-        self.set_group_start_to_current_pose()
-        self.set_target()
-        self.group.set_planning_time(15)
+        self.group.set_pose_target(self.start_pose)
+        self.group.set_planning_time(10)
+        print('moving to start')
         plan = self.group.plan()
         self.group.execute(plan)
-        rospy.spin()
+        print('at start')
+        self.rate.sleep()
         return
 
-    def move_target_constrained(self):
-        self.group.clear_pose_targets()
-        self.set_group_start_to_current_pose()
-        self.set_target()
-        self.group.set_path_constraints(self.get_constraint())
-        self.group.set_planning_time(50)
-        plan = self.group.plan()
-        self.group.execute(plan)
-        rospy.spin()
-        return
-
-    def get_constraint(self, euler_orientation = [0,.5,0], tol = [1,1,1]):
+    def get_constraint(self, euler_orientation = [0,math.pi/2,0], tol = [.5,.5,1]):
+        #method takes euler-angle inputs, this converts it to a quaternion
         q_orientation = tf.transformations.quaternion_from_euler(euler_orientation[0],euler_orientation[1],euler_orientation[2])
         orientation_msg = Quaternion(q_orientation[0],q_orientation[1],q_orientation[2],q_orientation[3])
-
+        #defines a constraint that sets the end-effector so a cup in it's hand will be upright, or straight upside-down
         constraint = Constraints()
         constraint.name = 'upright_wrist'
-
         upright_orientation = OrientationConstraint()
         upright_orientation.link_name = self.group.get_end_effector_link()
         upright_orientation.orientation = orientation_msg
@@ -207,7 +188,7 @@ class MoveCup():
         upright_orientation.absolute_y_axis_tolerance = tol[1]
         upright_orientation.absolute_z_axis_tolerance = tol[2]
         upright_orientation.weight = 1.0
-        upright_orientation.header = self.p.header
+        upright_orientation.header = self.start_pose.header
         constraint.orientation_constraints.append(upright_orientation)
         return(constraint)
 
@@ -217,9 +198,15 @@ if __name__ == '__main__':
     try:
         mover = MoveCup()
         while not rospy.is_shutdown():
+            #enables the robot
             mover.start_baxter_interface()
+            #moves the robot to a starting pose that makes future moves fail less
             mover.set_neutral()
-            mover.move_target_constrained()
-            rospy.sleep(10)
+            #slows down the robot path plans
+            mover.scale_movegroup()
+            mover.move_start()
+            #sets up the subscriber for the callback, currently set to take a pose
+            rospy.Subscriber('target_poses', PoseStamped, mover.callback)
+            rospy.spin()
     except rospy.ROSInterruptException:
         pass
